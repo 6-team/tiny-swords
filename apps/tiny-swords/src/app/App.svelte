051a46371147dcd78515f8e76edeccb3df01a0b0
Vladislav Khomenko <vladislav.khomenko@fairmarkit.com>
2023-08-26 12:38:31 +0300

    Set up project

---
+++ apps/tiny-swords/src/app/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Router, links, Route } from "svelte-routing";
+
+  import Generator from "./generator/Generator.svelte";
+</script>
+
+<div use:links>
+  <Router url="">
+    <Route path="/" component={Generator} />
+  </Router>
+</div>
