9f953a7f7d1bebb1898f7f6585060ad318287def
Белоусов Алексей Сергеевич (4046009) <a.belousov@vtb.ru>
2023-08-30 12:43:45 +0300

    add deco and change base map

--- apps/tiny-swords/src/app/generator/Generator.svelte
+++ apps/tiny-swords/src/app/generator/Generator.svelte
@@ -4,28 +4,50 @@
   import { TileName } from "../entites/scene/scene.const";
 
   const waterMap = [
-    new Array(15).fill(TileName.WATER_MIDDLE_MIDDLE),
-    new Array(15).fill(TileName.WATER_MIDDLE_MIDDLE),
-    new Array(15).fill(TileName.WATER_MIDDLE_MIDDLE),
-    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(11).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
-    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(11).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
-    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(11).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
-    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(11).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
-    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(11).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
-    new Array(15).fill(TileName.WATER_MIDDLE_MIDDLE),
-    new Array(15).fill(TileName.WATER_MIDDLE_MIDDLE),
+    new Array(20).fill(TileName.WATER_MIDDLE_MIDDLE),
+    new Array(20).fill(TileName.WATER_MIDDLE_MIDDLE),
+    new Array(20).fill(TileName.WATER_MIDDLE_MIDDLE),
+    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(16).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
+    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(16).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
+    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(16).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
+    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(16).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
+    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(16).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
+    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(16).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
+    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(16).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
+    [...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE), ...new Array(16).fill(null), ...new Array(2).fill(TileName.WATER_MIDDLE_MIDDLE)],
+    new Array(20).fill(TileName.WATER_MIDDLE_MIDDLE),
+    new Array(20).fill(TileName.WATER_MIDDLE_MIDDLE),
   ];
 
+  const foamMap = [
+    [],
+    [],
+    [null,               TileName.FOAM_TOP,                           ...new Array(16).fill(null), TileName.FOAM_TOP],
+    [TileName.FOAM_LEFT, TileName.FOAM_MIDDLE,                        ...new Array(16).fill(null), TileName.FOAM_MIDDLE, TileName.FOAM_RIGHT],
+    [TileName.FOAM_LEFT, TileName.FOAM_MIDDLE,                        ...new Array(16).fill(null), TileName.FOAM_MIDDLE, TileName.FOAM_RIGHT],
+    [TileName.FOAM_LEFT, TileName.FOAM_MIDDLE,                        ...new Array(16).fill(null), TileName.FOAM_MIDDLE, TileName.FOAM_RIGHT],
+    [TileName.FOAM_LEFT, TileName.FOAM_MIDDLE,                        ...new Array(16).fill(null), TileName.FOAM_MIDDLE, TileName.FOAM_RIGHT],
+    [TileName.FOAM_LEFT, TileName.FOAM_MIDDLE,                        ...new Array(16).fill(null), TileName.FOAM_MIDDLE, TileName.FOAM_RIGHT],
+    [TileName.FOAM_LEFT, TileName.FOAM_MIDDLE,                        ...new Array(16).fill(null), TileName.FOAM_MIDDLE, TileName.FOAM_RIGHT],
+    [TileName.FOAM_LEFT, TileName.FOAM_MIDDLE,                        ...new Array(16).fill(null), TileName.FOAM_MIDDLE, TileName.FOAM_RIGHT],
+    [TileName.FOAM_LEFT, TileName.FOAM_MIDDLE,                        ...new Array(16).fill(null), TileName.FOAM_MIDDLE, TileName.FOAM_RIGHT],
+    [TileName.FOAM_LEFT, ...new Array(18).fill(TileName.FOAM_MIDDLE), TileName.FOAM_RIGHT],
+    [null,               ...new Array(18).fill(TileName.FOAM_BOTTOM)],
+  ];
+  
   const sandMap = [
     [],
     [],
     [],
-    [null, TileName.SAND_TOP_LEFT,    ...new Array(11).fill(null), TileName.SAND_TOP_RIGHT],
-    [null, TileName.SAND_MIDDLE_LEFT, ...new Array(11).fill(null), TileName.SAND_MIDDLE_RIGHT],
-    [null, TileName.SAND_MIDDLE_LEFT, ...new Array(11).fill(null), TileName.SAND_MIDDLE_RIGHT],
-    [null, TileName.SAND_MIDDLE_LEFT, ...new Array(11).fill(null), TileName.SAND_MIDDLE_RIGHT],
-    [null, TileName.SAND_MIDDLE_LEFT, ...new Array(11).fill(TileName.SAND_MIDDLE_MIDDLE), TileName.SAND_MIDDLE_RIGHT],
-    [null, TileName.SAND_BOTTOM_LEFT, ...new Array(11).fill(TileName.SAND_BOTTOM_MIDDLE), TileName.SAND_BOTTOM_RIGHT],
+    [null, TileName.SAND_TOP_LEFT,    TileName.SAND_TOP_MIDDLE,    ...new Array(14).fill(null), TileName.SAND_TOP_MIDDLE,    TileName.SAND_TOP_RIGHT],
+    [null, TileName.SAND_MIDDLE_LEFT, TileName.SAND_MIDDLE_MIDDLE, ...new Array(14).fill(null), TileName.SAND_MIDDLE_MIDDLE, TileName.SAND_MIDDLE_RIGHT],
+    [null, TileName.SAND_MIDDLE_LEFT, TileName.SAND_MIDDLE_MIDDLE, ...new Array(14).fill(null), TileName.SAND_MIDDLE_MIDDLE, TileName.SAND_MIDDLE_RIGHT],
+    [null, TileName.SAND_MIDDLE_LEFT, TileName.SAND_MIDDLE_MIDDLE, ...new Array(14).fill(null), TileName.SAND_MIDDLE_MIDDLE, TileName.SAND_MIDDLE_RIGHT],
+    [null, TileName.SAND_MIDDLE_LEFT, TileName.SAND_MIDDLE_MIDDLE, ...new Array(14).fill(null), TileName.SAND_MIDDLE_MIDDLE, TileName.SAND_MIDDLE_RIGHT],
+    [null, TileName.SAND_MIDDLE_LEFT, TileName.SAND_MIDDLE_MIDDLE, ...new Array(14).fill(null), TileName.SAND_MIDDLE_MIDDLE, TileName.SAND_MIDDLE_RIGHT],
+    [null, TileName.SAND_MIDDLE_LEFT, TileName.SAND_MIDDLE_MIDDLE, ...new Array(14).fill(null), TileName.SAND_MIDDLE_MIDDLE, TileName.SAND_MIDDLE_RIGHT],
+    [null, TileName.SAND_MIDDLE_LEFT, ...new Array(16).fill(TileName.SAND_MIDDLE_MIDDLE), TileName.SAND_MIDDLE_RIGHT],
+    [null, TileName.SAND_BOTTOM_LEFT, ...new Array(16).fill(TileName.SAND_BOTTOM_MIDDLE), TileName.SAND_BOTTOM_RIGHT],
   ]
 
   const elevationMap = [
@@ -35,30 +57,64 @@
     [],
     [],
     [],
-    [null, null, TileName.ELEVATION_BOTTOM_LEFT, ...new Array(9).fill(TileName.ELEVATION_BOTTOM_MIDDLE), TileName.ELEVATION_BOTTOM_RIGHT],
-    [null, null, TileName.ELEVATION_EDGE_LEFT,   ...new Array(9).fill(TileName.ELEVATION_EDGE_MIDDLE),   TileName.ELEVATION_EDGE_RIGHT],
+    [],
+    [],
+    [],
+    [null, null, TileName.ELEVATION_BOTTOM_LEFT, ...new Array(14).fill(TileName.ELEVATION_BOTTOM_MIDDLE), TileName.ELEVATION_BOTTOM_RIGHT],
+    [null, null, TileName.ELEVATION_EDGE_LEFT,   ...new Array(14).fill(TileName.ELEVATION_EDGE_MIDDLE),   TileName.ELEVATION_EDGE_RIGHT],
   ];
 
   const groundMap = [
     [],
     [],
-    [null, null, TileName.GROUND_TOP_LEFT,    ...new Array(9).fill(TileName.GROUND_TOP_MIDDLE),    TileName.GROUND_TOP_RIGHT],
-    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(9).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
-    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(9).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
-    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(9).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
-    [null, null, TileName.GROUND_BOTTOM_LEFT, ...new Array(9).fill(TileName.GROUND_BOTTOM_MIDDLE), TileName.GROUND_BOTTOM_RIGHT],
+    [null, null, TileName.GROUND_TOP_LEFT,    ...new Array(14).fill(TileName.GROUND_TOP_MIDDLE),    TileName.GROUND_TOP_RIGHT],
+    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(14).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
+    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(14).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
+    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(14).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
+    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(14).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
+    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(14).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
+    [null, null, TileName.GROUND_MIDDLE_LEFT, ...new Array(14).fill(TileName.GROUND_MIDDLE_MIDDLE), TileName.GROUND_MIDDLE_RIGHT],
+    [null, null, TileName.GROUND_BOTTOM_LEFT, ...new Array(14).fill(TileName.GROUND_BOTTOM_MIDDLE), TileName.GROUND_BOTTOM_RIGHT],
+  ];
+
+  const bridgeMap = [
+    [],
+    [],
+    [],
+    [],
+    [TileName.BRIDGE_MIDDLE, TileName.BRIDGE_MIDDLE, TileName.BRIDGE_RIGHT],
+    [TileName.BRIDGE_SHADOW, TileName.BRIDGE_SHADOW],
+    [],
+    [...new Array(17).fill(null), TileName.BRIDGE_LEFT, TileName.BRIDGE_MIDDLE, TileName.BRIDGE_MIDDLE],
+    [...new Array(18).fill(null), TileName.BRIDGE_SHADOW, TileName.BRIDGE_SHADOW],
+  ];
+
+  const decoMap = [
+    [],
+    [],
+    [...new Array(9).fill(null), TileName.DECO_MUSHROOM_L, TileName.DECO_MUSHROOM_M],
+    [...new Array(10).fill(null), TileName.DECO_MUSHROOM_S, TileName.DECO_MUSHROOM_S],
+    [...new Array(7).fill(null), TileName.DECO_STONE_S],
+    [...new Array(4).fill(null), TileName.DECO_BUSH_M],
+    [...new Array(5).fill(null), TileName.DECO_PUMPKIN_S, TileName.DECO_BUSH_L],
+    [...new Array(5).fill(null), TileName.DECO_STONE_L, ...new Array(4).fill(null), TileName.DECO_WEED_M],
+    [...new Array(9).fill(null), TileName.DECO_WEED_S, ...new Array(7).fill(null), TileName.DECO_BONE_S],
+    [...new Array(7).fill(null), TileName.DECO_STONE_M, ...new Array(7).fill(null),  TileName.DECO_BONE_M],
   ];
 
   onMount(async () => {
-    const scene = new Scene({ tileSize: 64, scale: 1 });
+    const scene = new Scene({ tileSize: 64, scale: 0.75 });
 
     await scene.renderLayer(waterMap);
+    await scene.renderLayer(foamMap);
     await scene.renderLayer(sandMap);
     await scene.renderLayer(elevationMap);
     await scene.renderLayer(groundMap);
+    await scene.renderLayer(bridgeMap);
+    await scene.renderLayer(decoMap);
   });
 </script>
 
 <div>
-  <canvas id="canvas" width="1000" height="700"></canvas>
+  <canvas id="canvas" width="960" height="720"></canvas>
 </div>
