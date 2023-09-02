66e63b547510434e6eb4c49f24e52be2e0dad795
Nikolaikolya <starcrafters@ya.ru>
2023-09-02 12:04:20 +0700

    Fix LevelTile class

--- apps/tiny-swords/src/app/generator/Generator.svelte
+++ apps/tiny-swords/src/app/generator/Generator.svelte
@@ -3,21 +3,12 @@
   import { Scene } from '../entites/scene/scene';
   import {generate} from "./generate";
 
-  let scene = null;
-
-
-  const clickAction = async () => {
-    await scene.renderLayer(generate());
-  }
-
   onMount(async () => {
-    scene = new Scene({ tileSize: 64, scale: 0.75 });
+    const scene = new Scene({ tileSize: 64, scale: 0.75 });
     await scene.renderLayer(generate());
   });
 </script>
 
 <div>
   <canvas id="canvas" width="960" height="720"></canvas>
-
-  <button on:click={clickAction}>Re Render</button>
 </div>
