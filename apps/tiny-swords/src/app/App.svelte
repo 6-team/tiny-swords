453980d4f52519891b25ba06377265d52629762b
Белоусов Алексей Сергеевич (4046009) <a.belousov@vtb.ru>
2023-09-04 22:12:40 +0300

    add prototype of wfc

--- apps/tiny-swords/src/app/App.svelte
+++ apps/tiny-swords/src/app/App.svelte
@@ -1,11 +1,11 @@
 <script>
   import { Router, links, Route } from "svelte-routing";
 
-  import Generator from "./generator/Generator.svelte";
+  import WFC_ref from "./generator/WFC_ref.svelte";
 </script>
 
 <div use:links>
   <Router url="">
-    <Route path="/" component={Generator} />
+    <Route path="/" component={WFC_ref} />
   </Router>
 </div>
