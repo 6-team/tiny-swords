04caa626fcaa31de9141edd3791d049501fdb1ce
Aliaksei Paliachynski <aliaksei.paliachynski@startmatter.com>
2023-09-12 10:15:26 +0300

    add hero menu

--- apps/tiny-swords/src/app/entites/renderer/renderer.ts
+++ apps/tiny-swords/src/app/entites/renderer/renderer.ts
@@ -12,6 +12,14 @@ function* enumerate<T>(iterable: Iterable<T>): Iterable<[number, T]> {
   }
 }
 
+async function loadImage(src: string): Promise<HTMLImageElement> {
+  return new Promise((resolve) => {
+    const image = new Image();
+    image.onload = () => resolve(image);
+    image.src = src;
+  });
+}
+
 /**
  * Класс, отвечающий за рендер элементов в canvas.
  * По идее ничего не знает про размеры тайлов и каким координатам в px соответствуют номера тайлов.
@@ -61,7 +69,6 @@ export class Renderer {
           .map((name) => mapTileNameToClass[name])
           .map((data) => {
             const { constructor: Constructor, args } = data;
-
             return this.render(this.#system.transformToPixels(tileNameIndex, rowIndex, 1, 1), new Constructor(...args));
           })
           .extract();
@@ -81,6 +88,75 @@ export class Renderer {
 
       this.render(this.#system.transformToPixels(coords[0], coords[1], sizes[0], sizes[1]), interactive);
     }
+  }
+
+  async renderHealthBar(lives: { totalLives: number; availableLives: number; blockedLives: number }) {
+    const { totalLives, availableLives, blockedLives } = lives;
+    const heartWidth = 20;
+    const heartHeight = 20;
+    const heartgPadding = 10;
+    const backgroundWidth = 128;
+    const backgroundHeight = 42;
+    const barPositionX = 800;
+    const barPositionY = 20;
+
+    const [backgroundImage, heartImage, wastedHeartImage, lockedHeartImage] = await Promise.all([
+      loadImage('img/UI/Ribbon_Yellow_3Slides.png'),
+      loadImage('img/UI/1.png'),
+      loadImage('img/UI/1-1.png'),
+      loadImage('img/UI/Regular_10.png'),
+    ]);
+
+    this.#context.drawImage(backgroundImage, barPositionX, barPositionY, backgroundWidth, backgroundHeight);
+
+    for (let i = 0; i < totalLives; i++) {
+      let heart;
+      if (i < availableLives) {
+        heart = heartImage;
+      } else if (i < availableLives + blockedLives) {
+        heart = wastedHeartImage;
+      } else {
+        heart = lockedHeartImage;
+      }
+
+      this.#context.drawImage(
+        heart,
+        i * (heartWidth + heartgPadding) + barPositionX + 22,
+        barPositionY + 5,
+        heartWidth,
+        heartHeight,
+      );
+    }
+    return this;
+  }
+
+  async renderResourcesBar(resources: Array<{ type: string; count: number; image: string }>) {
+    const backgroundImage = await loadImage('img/UI/Banner_Connection_Left.png');
+    const hPadding = 30;
+    const vPadding = 10;
+    const backgroundWidth = 85;
+    const backgroundHeight = 100;
+
+    this.#context.font = '18px vinque';
+    this.#context.fillStyle = 'SaddleBrown';
+
+    const maxWidthText = Math.max(...resources.map(({ count }) => this.#context.measureText(String(count)).width));
+    this.#context.drawImage(
+      backgroundImage,
+      50,
+      20,
+      backgroundWidth + maxWidthText + maxWidthText / 2,
+      backgroundHeight,
+    );
+
+    await Promise.all(
+      resources.map(async (resource, i) => {
+        const resourceImage = await loadImage(resource.image);
+        this.#context.fillText(String(resource.count), 115 + maxWidthText / 5, i * hPadding + 55);
+        this.#context.drawImage(resourceImage, vPadding + 50 + maxWidthText / 5, i * hPadding + 5, 70, 70);
+      }),
+    );
+    return this;
   }
 
   get interactives() {
