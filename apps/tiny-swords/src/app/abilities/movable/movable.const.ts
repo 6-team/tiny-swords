63bba008a3e8345df3fdc5866b444bcf516bac6f
Veniamin Trepachko <trepven@gmail.com>
2023-09-19 16:23:09 +0300

    fix: change character speed

--- apps/tiny-swords/src/app/abilities/movable/movable.const.ts
+++ apps/tiny-swords/src/app/abilities/movable/movable.const.ts
@@ -4,10 +4,12 @@ export const enum MovingError {
   PERSONAGE_NOT_SET = "Can't call Moving methods without personage",
 }
 
+export const PIXELS_PER_FRAME = 2;
+
 export const movementSetters: Record<MovingDirection, (value: [number, number]) => [number, number]> = {
-  [MovingDirection.UP]: ([prevX, prevY]) => [prevX, prevY - 1],
-  [MovingDirection.DOWN]: ([prevX, prevY]) => [prevX, prevY + 1],
-  [MovingDirection.LEFT]: ([prevX, prevY]) => [prevX - 1, prevY],
-  [MovingDirection.RIGHT]: ([prevX, prevY]) => [prevX + 1, prevY],
+  [MovingDirection.UP]: ([prevX, prevY]) => [prevX, prevY - PIXELS_PER_FRAME],
+  [MovingDirection.DOWN]: ([prevX, prevY]) => [prevX, prevY + PIXELS_PER_FRAME],
+  [MovingDirection.LEFT]: ([prevX, prevY]) => [prevX - PIXELS_PER_FRAME, prevY],
+  [MovingDirection.RIGHT]: ([prevX, prevY]) => [prevX + PIXELS_PER_FRAME, prevY],
   [MovingDirection.IDLE]: (coords) => coords,
 };
