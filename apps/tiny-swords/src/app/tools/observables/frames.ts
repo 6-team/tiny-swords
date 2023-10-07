bcd75c9ab8b29a6313752582cc9cdb9c66dc9f5c
Veniamin Trepachko <trepven@gmail.com>
2023-10-07 12:31:47 +0300

    fix: add min frame time

--- apps/tiny-swords/src/app/tools/observables/frames.ts
+++ apps/tiny-swords/src/app/tools/observables/frames.ts
@@ -2,10 +2,18 @@ import { Subject } from 'rxjs';
 
 const observable = new Subject<number>();
 
-function raf() {
-  requestAnimationFrame((number) => {
-    observable.next(number);
+const MIN_FRAME_MS = 16;
 
+function raf() {
+  const timeoutPromise = new Promise((resolve) => {
+    setTimeout(() => resolve(Number(Date.now())), MIN_FRAME_MS);
+  });
+  const rafPromise = new Promise((resolve) => {
+    requestAnimationFrame(() => resolve(Number(Date.now())));
+  });
+
+  Promise.race([timeoutPromise, rafPromise]).then((timestamp: number) => {
+    observable.next(timestamp);
     raf();
   });
 }
@@ -13,3 +21,11 @@ function raf() {
 raf();
 
 export const frames$ = observable.asObservable();
+
+let prev = Number(new Date());
+
+frames$.subscribe((frame) => {
+  const next = Number(new Date());
+  console.log(next - prev);
+  prev = next;
+});
