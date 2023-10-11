8ed5740d091a9d9c8c6cb270078e1132f6acaa38
Artemiy Orlov <artemiyo.dev@gmail.com>
2023-10-11 19:40:36 +0300

    remove routing

--- apps/tiny-swords/src/app/App.svelte
+++ apps/tiny-swords/src/app/App.svelte
@@ -1,14 +1,9 @@
 <script>
-  import { Router, links, Route } from "svelte-routing";
 
   import Generator from "./generator/Generator.svelte";
 </script>
 
-<div use:links>
-  <Router url="">
-    <Route path="/" component={Generator} />
-  </Router>
-</div>
+<Generator />
 
 <style>
   :global(*) {
