8234078c885f17d2d932379d2ed2c575cb475c29
Белоусов Алексей Сергеевич (4046009) <a.belousov@vtb.ru>
2023-10-20 22:56:06 +0300

    change logic for move enemy

--- apps/tiny-swords/src/app/core/actions/actions.ts
+++ apps/tiny-swords/src/app/core/actions/actions.ts
@@ -7,7 +7,7 @@ import { LayersMap } from '../layers/layers.types';
 import { Resource } from '../../entities/resource';
 
 // TODO: need to move
-const ENDPOINT = 'ws://localhost:3000';
+const ENDPOINT = 'ws://localhost:3016';
 
 class Actions<T extends IPlayer<MovingDirection>, L extends LevelData<LayersMap, Resource>> {
   #socketSubject = new BehaviorSubject<WebSocket>(null);
